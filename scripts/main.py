a7397a0a208a88f770855bb9f2b2f0121b55cf11
therinMody <therinharmenraymody@gmail.com>
2024-07-12 14:58:05 -0600

    Built out lib folder

--- scripts/main.py
+++ scripts/main.py
@@ -7,7 +7,8 @@ def main():
     directory_path = './lib'
         
     for i in range(6, 17):
-        file_path = directory_path+"/0"+str(i)+"_rounds"
+        folder_name = f"{i:02}_rounds"
+        file_path = os.path.join(directory_path, folder_name)
         
         if not os.path.exists(file_path):
             os.makedirs(file_path)
