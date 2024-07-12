82accdb5fcd5b7a538cbb07c9cec7671e4ab6284
therinMody <therinharmenraymody@gmail.com>
2024-07-12 14:48:50 -0600

    Built out lib folder

--- scripts/functions/calculate_prob_not_black.py
+++
@@ -1,4 +0,0 @@
-def calculate_prob_not_black(spins, total_slots=38, black_slots=18):
-    not_black_slots = total_slots - black_slots
-    prob_not_black_one_spin = not_black_slots / total_slots
-    return prob_not_black_one_spin ** spins
--- scripts/functions/headers.py
+++ scripts/functions/headers.py
@@ -1,19 +1,13 @@
-# headers.py
-
 def headers(table, widths):
     header_titles = ["Max Bet", "Round", "Bet", "Profit - Running Loss", "Running Loss", "Prob. No Black (%)"]
-    header_underlines = [""] * len(header_titles)  # Create an empty list for underlines
-
-    # Generate headers with specific widths
+    
+    # Create the header row
+    header_row = '|'
+    separator_row = '|'
     for index, title in enumerate(header_titles):
-        header_underlines[index] = "-" * widths[index]  # Underlines
-        table.append(f"| {title.center(widths[index])} ")  # Center align title within its width
-
-    table.append("|\n")  # End of header row
-    table.append("+")  # Start the bottom line of the header
-
-    # Generate the bottom line of the header
-    for width in widths:
-        table.append("-" * (width + 2) + "+")  # "+2" accounts for padding spaces around the text
-
-    table.append("\n")
+        header_row += f" {title.center(widths[index])} |"
+        separator_row += '-' * (widths[index] + 2) + '|'
+    
+    # Append rows to the table list
+    table.append(header_row + '\n')
+    table.append(separator_row + '\n')
